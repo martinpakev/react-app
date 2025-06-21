bafcaa7350f11fbbc2098c9514ac733ad5bf826a
martinpakev <marti_pakev@abv.bg>
2025-06-21 12:47:59 +0300

    ListGroup included in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import Button from "./components/Button";
 import Alert from "./components/Alert";
 import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 
 function App() {
  const [alertVisible, setAlertVisability] = useState(false);
+
+ let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+const handleSelectItem = (item:string) =>{
+  console.log(item);
+}
+
 
   return (
      <div>
       {alertVisible && <Alert onClose={() => setAlertVisability(false)}>My alert</Alert>}
       <Button color="secondary" onClick={() => setAlertVisability(true)} > Exercise</Button>
+      <ListGroup items={items} heading="Citites" onSelectItem={handleSelectItem}/>
      </div>
   ) 
     
@@ -16,13 +25,5 @@ function App() {
 
 export default App;
 
-// let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-// const handleSelectItem = (item:string) =>{
-//   console.log(item);
-// }
 
-// return (
-//   <div>
-//     <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-//   </div>
